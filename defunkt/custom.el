c9d15a2b0c82d642afb596c2aed46d041003c6a5
Bryce Kerley <bryce@worldmedia.net>
2009-02-10 12:35:51 -0500

    fonts

--- defunkt/custom.el
+++ defunkt/custom.el
@@ -14,15 +14,14 @@
  '(js2-bounce-indent-flag t)
  '(js2-enter-indents-newline t)
  '(js2-strict-missing-semi-warning nil)
- '(tabbar-mode nil nil (tabbar))
  '(transient-mark-mode t))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 160 :width normal :family "apple-inconsolata"))))
- '(emacs-lisp-mode-default ((t (:inherit autoface-default :slant normal :weight normal :height 160 :family "inconsolata"))) t)
+ '(default ((t (:stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :family "monaco"))))
+ '(emacs-lisp-mode-default ((t (:inherit autoface-default :slant normal :weight normal))) t)
  '(minibuffer-prompt ((((background dark)) (:foreground "cyan" :height 160))))
- '(mode-line ((t (:inherit aquamacs-variable-width :background "grey75" :foreground "black" :width normal))))
- '(text-mode-default ((t (:inherit autoface-default :strike-through nil :underline nil :slant normal :weight normal :height 160 :width normal :family "inconsolata"))) t))
+ '(mode-line ((t (:inherit aquamacs-variable-width :background "grey25" :foreground "grey75" :width normal))))
+ '(text-mode-default ((t (:inherit autoface-default :strike-through nil :underline nil :slant normal :weight normal :width normal))) t))
