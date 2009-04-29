15e23389b90530eda80b270f53d034b2bb9fd10e
Janmejay Singh <singh.janmejay@gmail.com>
2009-04-29 15:53:53 +0530

    enabled reading atom feeds on gnus -jj

--- custom/gnus.el
+++ custom/gnus.el
@@ -11,3 +11,19 @@
 (add-hook 'gnus-summary-mode-hook 
           (lambda ()
             (local-set-key (kbd "<backspace>") 'gnus-move-to-trash)))
+
+(require 'mm-url)
+(defadvice mm-url-insert (after DE-convert-atom-to-rss () )
+  "Converts atom to RSS by calling xsltproc."
+  (when (re-search-forward "xmlns=\"http://www.w3.org/.*/Atom\"" 
+                           nil t)
+    (message "Converting Atom to RSS... ")
+    (goto-char (point-min))
+    (call-process-region (point-min) (point-max) 
+                         "xsltproc" 
+                         t t nil 
+                         (expand-file-name "~/.emacs.d/vendor/atom2rss.xsl") "-")
+    (goto-char (point-min))
+    (message "Converting Atom to RSS... done")))
+
+(ad-activate 'mm-url-insert)
