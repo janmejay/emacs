2b7aa2a47f507e229434105c9d25cc0ec893a086
Janmejay Singh <singh.janmejay@gmail.com>
2009-08-05 04:10:32 +0530

    pulled custom variables out of view.el to a dedicated file

---
+++ custom/var_customization.el
@@ -0,0 +1,28 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ecb-layout-window-sizes (quote (("left3" (0.1921182266009852 . 0.35294117647058826) (0.1921182266009852 . 0.3137254901960784) (0.1921182266009852 . 0.3137254901960784)))))
+ '(ecb-options-version "2.33beta2")
+ '(ecb-source-path (find-all-emacs-projects))
+ '(ecb-tip-of-the-day nil)
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil)
+ '(case-fold-search nil)
+ '(cua-mode nil nil (cua-base))
+ '(jde-jdk-registry (quote (("1.6" . "/usr/local/jdk_1_6/bin/java"))))
+ '(jde-gen-k&r t)
+ '(transient-mark-mode t)
+ '(show-paren-mode t)
+ '(semanticdb-default-file-name ".semantic.cache")
+ '(semanticdb-default-save-directory "~/.emacs.d/.semantic"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 60 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
+
+
+(setq junit-jar-location "~/projects/code_review/junit-4.3.1.jar")
--- custom/view.el
+++ custom/view.el
@@ -27,29 +27,7 @@
     (enable-ecb-and-switch-to-dir-window))
   (setq ecb-is-enabled (not ecb-is-enabled)))
 
-(custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(ecb-layout-window-sizes (quote (("left3" (0.1921182266009852 . 0.35294117647058826) (0.1921182266009852 . 0.3137254901960784) (0.1921182266009852 . 0.3137254901960784)))))
- '(ecb-options-version "2.33beta2")
- '(ecb-source-path (find-all-emacs-projects))
- '(ecb-tip-of-the-day nil)
- '(scroll-bar-mode nil)
- '(tool-bar-mode nil)
- '(case-fold-search nil)
- '(cua-mode nil nil (cua-base))
- '(transient-mark-mode t)
- '(show-paren-mode t)
- '(semanticdb-default-file-name ".semantic.cache")
- '(semanticdb-default-save-directory "~/.emacs.d/.semantic"))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 60 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
+(load "~/.emacs.d/custom/var_customization.el")
 
 (ecb-layout-switch "left3")
 ;;(ecb-activate)
