2079068bddd95c335c92abe518f668fb39e1c415
Janmejay Singh <singh.janmejay@gmail.com>
2009-09-24 16:22:58 -0700

    added a little utility to devide screen to monitor irc channels

--- custom/irc-startup.el
+++ custom/irc-startup.el
@@ -22,15 +22,38 @@
 
 (setq rcirc-server-alist 
       '(("irc.freenode.net" :channels
-         ("#emacs" "#debian" "#rubyonrails" "#ruby" "#gosu"))
-        ("localhost"))) 
+         ("#gentoo" "#debian" "#emacs" "#gosu" "#rubyonrails" "#ruby")))) 
 
 
 ;; Connect to servers.
 ;(rcirc); freenode is the default
 ;(rcirc-connect "localhost"); if you run bitlbee, this will connect to it
 
-(defun bring-forth-all-irc-windows ()
+(defun unleash-irc ()
+  (interactive)
+  (delete-other-windows)
+  (layout-irc-windows-all-over-the-window))
+
+(defun layout-irc-windows-all-over-the-window ()
+  (let ((irc-buffer-names (flattened-channel-buffer-list rcirc-server-alist '())))
+    (let ((width (/ (window-width) (/ (+ (length irc-buffer-names) 1) 2)))) 
+      (create-columns-with-2-chanels-each irc-buffer-names (selected-window) width))))
+
+(defun create-columns-with-2-chanels-each (buffer-list splittable column-width)
+  (set-window-buffer splittable (car buffer-list))
+  (if (> (length buffer-list) 2) 
+      (let ((new-window (split-window splittable column-width t)))
+        (create-columns-with-2-chanels-each (cddr buffer-list) new-window column-width)))
+  (let ((bottom-buffer (cadr buffer-list)))
+    (if bottom-buffer (set-window-buffer (split-window splittable) bottom-buffer))))
+
+(defun flattened-channel-buffer-list (servers-alist populate-into)
+  (let ((domain (caar servers-alist)) (channels (caddar servers-alist)) (more-irc-servers (cdr servers-alist)))
+    (let ((buffer-names (mapcar (lambda (channel) (concat channel "@" domain)) channels)))
+      (append-to-list 'populate-into buffer-names)
+      (if (> (length more-irc-servers) 0) (flattened-channel-buffer-list more-irc-servers populate-into) populate-into))))
+
+(defun bring-forth-all-channels-on-side ()
   (interactive)
   (delete-other-windows)
   (being-forth-all-irc-channels rcirc-server-alist))
